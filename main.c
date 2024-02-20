17976dd9155759b5f0ad65bcf7cb40144ebdf824
smgibson99 <smgibson@gibsontechnical.com>
2024-02-19 17:25:47 -0800

    add base4 functions

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "3DES.h"
+#include "base64.h"
 
 int main(int argc, char**argv) {
 
@@ -23,17 +24,31 @@ int main(int argc, char**argv) {
 	//50, 51, 52, 53, 54, 55, 56, 57
 	unsigned char* ivString = "2345679";
 	unsigned char iv[8] = "2345679";
+	unsigned char iv2[8] = "2345679";
 
-    unsigned char *clearText = "This is a password";
+    unsigned char clearText[25] = "This is a password";
     int textLength = strlen(clearText);
-
+	
 	int result;
 
-    unsigned char cipherText[255] = {0};
-    int cipherLength = strlen(cipherText);
+	unsigned char cipher[255] = {0};
+    int cipherLength = 0;
+	unsigned char *cipherText = NULL;
 
-    result = des3_cbc_encrypt(cipherText, clearText, textLength, keyString, keyLength, iv);
-    result = des3_cbc_decrypt(clearText, cipherText, cipherLength, keyString, keyLength, iv);
+	printf("Clear Text to encrypt: %s\n", clearText);
+    result = des3_cbc_encrypt(cipher, clearText, textLength, keyString, keyLength, iv);
+  	cipherLength = strlen(cipher);
+	
+	cipherText = base64_encode(cipher, cipherLength, &cipherLength);
+
+	printf("Cipher Text: %s\n", cipherText);
+
+	clearText[0] = '\0';
+	cipherLength = strlen(cipher);
+
+    result = des3_cbc_decrypt(clearText, cipher, cipherLength, keyString, keyLength, iv2);
+
+	printf("Clear Text back from decryption: %s\n", clearText);
 
     return 0;
 }
